be308b4274ee8d5d74f0aea771b51a12d2876d0a
Caio <caio@exemplo.com>
2026-05-29 23:53:49 -0300

    UI: Increase particle speed slightly

--- src/components/NeonParticles.tsx
+++ src/components/NeonParticles.tsx
@@ -38,8 +38,8 @@ export default function NeonParticles() {
       particles = Array.from({ length: PARTICLE_COUNT }, () => ({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-        vx: (Math.random() - 0.5) * 0.2,
-        vy: -(Math.random() * 0.4 + 0.1),
+        vx: (Math.random() - 0.5) * 0.5,
+        vy: -(Math.random() * 0.8 + 0.3),
         radius: Math.random() * 1.5 + 0.5,
         alpha: Math.random() * 0.4 + 0.15,
         alphaDir: Math.random() > 0.5 ? 0.003 : -0.003,
